3d8c12135e8e9579c78982f7844208389184e5ab
Barham Farraj <barhamfarraj@icloud.com>
2022-04-14 03:41:48 +0300

    Done

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -15,7 +15,7 @@ class webScraper:
   def get_citations_needed_report(self):
     for paragraph in self.soup.find_all('p'):
       occurrences=paragraph.text.count('[citation needed]')
-      if '[citation needed]' in paragraph.text:
+      if '[citation needed]' in paragraph.text:  
         if occurrences >1:
           for i in range(occurrences):
             subpara = paragraph.text.strip().split('[citation needed]')[i]+'[citation needed]'
@@ -25,11 +25,12 @@ class webScraper:
           subpara = paragraph.text.strip().split('[citation needed]')[0]+'[citation needed]'
           print('\n',subpara,'\n')    
     return 
-     
+  
 if __name__== "__main__":
   URL = "https://en.wikipedia.org/wiki/History_of_Mexico"
   
   webScraper(URL).get_citations_needed_count()
   webScraper(URL).get_citations_needed_report()
+  # webScraper(URL).get_citations_needed_by_section()
   
   
