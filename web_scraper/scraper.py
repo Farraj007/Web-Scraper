5bc8487e29160eb4f32e8a82eba8e60de36d6f6b
Barham Farraj <barhamfarraj@icloud.com>
2022-04-14 03:15:16 +0300

    Done

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -6,20 +6,30 @@ class webScraper:
     self.URL= URL
     self.page= requests.get(URL)
     self.soup= BeautifulSoup(self.page.content, 'html.parser')
-    
-  def citation_needed(self):
-    counter = 0
-    
-    for pargraph in self.soup.find_all("p"):
-        z= re.findall(r'\[+citation needed+\]', pargraph.text)
-        counter += len(z)
-        if '[citation needed]' in pargraph.text:
-            line = pargraph.text
-            print('\n',line,'\n') 
-    print(counter)        
-    return line, counter
+    self.searchfor= self.soup.find_all(string=re.compile("citation needed"))
+  def get_citations_needed_count(self):
+    count=len(self.searchfor)
+    print(count)
+    return 
+  
+  def get_citations_needed_report(self):
+    for paragraph in self.soup.find_all('p'):
+      occurrences=paragraph.text.count('[citation needed]')
+      if '[citation needed]' in paragraph.text:
+        if occurrences >1:
+          for i in range(occurrences):
+            subpara = paragraph.text.strip().split('[citation needed]')[i]+'[citation needed]'
+            print('\n',subpara,'\n')    
+
+        else:
+          subpara = paragraph.text.strip().split('[citation needed]')[0]+'[citation needed]'
+          print('\n',subpara,'\n')    
+    return 
      
 if __name__== "__main__":
   URL = "https://en.wikipedia.org/wiki/History_of_Mexico"
-  webScraper(URL).citation_needed()
+  
+  webScraper(URL).get_citations_needed_count()
+  webScraper(URL).get_citations_needed_report()
+  
   
